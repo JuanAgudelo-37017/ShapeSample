dc993604fb42d194468babb25debfd15f27205e1
Juan <juan.agudelo37017@ucaldas.edu.co>
2022-09-23 09:40:38 -0500

    Agreguè la clase shape

---
+++ src/main/java/org/example/Shape.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public abstract class Shape {
+
+    public int x = 0;
+    private int y=0;
+
+    public int getX() {
+
+        return x;
+    }
+
+    public void setX(int newX) {
+        x = newX;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int newY) {
+        y = newY;
+    }
+
+    public abstract double getArea();
+    public abstract double getPerimeter();
+}
